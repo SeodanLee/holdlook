5fd8e32f7af3d1ed4faeff0db2bf6aa2ea34fe14
Cats <42129397+Cats1337@users.noreply.github.com>
2022-09-28 04:01:10 -0400

    Delete holdlook/src/main directory

--- holdlook/src/main/java/cats/holdlook/Reference.java
+++
@@ -1,7 +0,0 @@
-package cats.holdlook;
-
-public class Reference {
-	public static final String MOD_ID = "hlook";
-	public static final String MOD_VERSION = "1.1";
-	public static final String MOD_NAME = "Holdlook";
-}
